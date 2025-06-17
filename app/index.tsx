0c593d6a89b57d5a96a948619ed41129ded4faff
JaimeA <jaimeangelcabarios@gmail.com>
2025-06-16 21:44:52 -0400

    proyecto iniciado

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ScreenPrincipal = () => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>HOME</Text>
+        </View>
+    )
+}
+
+export default ScreenPrincipal
